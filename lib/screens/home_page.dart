4bcb995eea51acea64131ba528c50ad7f08e8df0
alpha-3000 <amonkar3000@gmail.com>
2022-01-30 16:20:41 +0530

    added location permission and access

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,6 +4,7 @@ import 'package:weather_app/components/current_weather.dart';
 import 'package:weather_app/components/current_weather_info.dart';
 import 'package:weather_app/model/current_weather_model.dart';
 import 'package:weather_app/services/currrent_weather_api.dart';
+import 'package:location/location.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,11 +15,19 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Future<CurrentWeather> currentWeather;
+  Location location = Location();
+
+  bool isLoading = true;
+
+  late bool _serviceEnabled;
+  late PermissionStatus _permissionGranted;
+  late LocationData _locationData;
 
   @override
   void initState() {
     super.initState();
-    currentWeather = fetchCurrentWeather();
+    getLocation();
+    // currentWeather = fetchCurrentWeather();
   }
 
   @override
@@ -41,81 +50,156 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: FutureBuilder<CurrentWeather>(
-          future: currentWeather,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  currentWeatherWidget(
-                    snapshot.data!.iconName.toString(),
-                    snapshot.data!.temp.toString(),
-                    snapshot.data!.cityName.toString(),
-                    snapshot.data!.country.toString(),
-                    snapshot.data!.description.toString(),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  currentWeatherInfo(
-                    "Wind",
-                    "Pressure",
-                    "Humidity",
-                    "Feels like",
-                    snapshot.data!.wind.toString(),
-                    snapshot.data!.pressure.toString(),
-                    snapshot.data!.humidity.toString(),
-                    snapshot.data!.feelsLike.toString(),
-                    "m/s",
-                    "hPa",
-                    "%",
-                    "℃",
-                  ),
-                  currentWeatherInfo(
-                    "Min Temp",
-                    "Max Temp",
-                    "Sunrise",
-                    "Sunset",
-                    snapshot.data!.minTemp.toString(),
-                    snapshot.data!.maxTemp.toString(),
-                    snapshot.data!.sunrise.toString(),
-                    snapshot.data!.sunset.toString(),
-                    "℃",
-                    "℃",
-                    "am",
-                    "pm",
-                  ),
-                ],
-              );
-            } else if (snapshot.hasError) {
-              return SizedBox(
-                height: MediaQuery.of(context).size.height / 1.3,
-                child: AlertDialog(
-                  title: const Text("Alert!!"),
-                  content: const Text("No internet connection"),
-                  actions: [
-                    ElevatedButton(
-                      child: const Text("OK"),
-                      onPressed: () {
-                        SystemChannels.platform
-                            .invokeMethod('SystemNavigator.pop');
-                      },
-                    ),
-                  ],
-                ),
-              );
-            } else {
-              return SizedBox(
+        child: isLoading
+            ? SizedBox(
                 height: MediaQuery.of(context).size.height / 1.3,
                 child: const Center(
                   child: CircularProgressIndicator(),
                 ),
-              );
-            }
-          },
-        ),
+              )
+            : FutureBuilder<CurrentWeather>(
+                future: currentWeather,
+                builder: (context, snapshot) {
+                  // print(snapshot.data!);
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        currentWeatherWidget(
+                          snapshot.data!.iconName.toString(),
+                          snapshot.data!.temp.toString(),
+                          snapshot.data!.cityName.toString(),
+                          snapshot.data!.country.toString(),
+                          snapshot.data!.description.toString(),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        currentWeatherInfo(
+                          "Wind",
+                          "Pressure",
+                          "Humidity",
+                          "Feels like",
+                          snapshot.data!.wind.toString(),
+                          snapshot.data!.pressure.toString(),
+                          snapshot.data!.humidity.toString(),
+                          snapshot.data!.feelsLike.toString(),
+                          "m/s",
+                          "hPa",
+                          "%",
+                          "℃",
+                        ),
+                        currentWeatherInfo(
+                          "Min Temp",
+                          "Max Temp",
+                          "Sunrise",
+                          "Sunset",
+                          snapshot.data!.minTemp.toString(),
+                          snapshot.data!.maxTemp.toString(),
+                          snapshot.data!.sunrise.toString(),
+                          snapshot.data!.sunset.toString(),
+                          "℃",
+                          "℃",
+                          "am",
+                          "pm",
+                        ),
+                      ],
+                    );
+                  }
+                  //else if (snapshot.hasError) {
+                  //   return SizedBox(
+                  //     height: MediaQuery.of(context).size.height / 1.3,
+                  //     child: AlertDialog(
+                  //       title: const Text("Alert!!"),
+                  //       content: const Text("No internet connection"),
+                  //       actions: [
+                  //         ElevatedButton(
+                  //           child: const Text("OK"),
+                  //           onPressed: () {
+                  //             SystemChannels.platform
+                  //                 .invokeMethod('SystemNavigator.pop');
+                  //           },
+                  //         ),
+                  //       ],
+                  //     ),
+                  //   );
+                  // }
+                  else {
+                    return SizedBox(
+                      height: MediaQuery.of(context).size.height / 1.3,
+                      child: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  }
+                },
+              ),
       ),
+    );
+  }
+
+  getLocation() async {
+    _serviceEnabled = await location.serviceEnabled();
+    print("Step 1");
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      print("Step 2");
+
+      if (!_serviceEnabled) {
+        print("Step 3");
+        SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    print("Step 4");
+
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      print("Step 5");
+
+      if (_permissionGranted != PermissionStatus.granted) {
+        print("Step 6");
+
+        // SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+        showAlertDialog(context);
+      }
+    }
+
+    _locationData = await location.getLocation();
+    // print(_locationData.longitude);
+
+    currentWeather =
+        fetchCurrentWeather(_locationData.latitude, _locationData.longitude);
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  showAlertDialog(BuildContext context) {
+    // Create button
+    Widget okButton = ElevatedButton(
+      child: const Text("OK"),
+      onPressed: () {
+        SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+      },
+    );
+
+    // Create AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: const Text("Alert"),
+      content: const Text("Please allow location manually"),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 }
--- lib/services/currrent_weather_api.dart
+++ lib/services/currrent_weather_api.dart
@@ -4,18 +4,19 @@ import 'package:http/http.dart' as http;
 import 'package:weather_app/config/api_config.dart';
 import 'package:weather_app/model/current_weather_model.dart';
 
-Future<CurrentWeather> fetchCurrentWeather() async {
+Future<CurrentWeather> fetchCurrentWeather(latitute,longitute) async {
   try {
     final response = await http.get(
       Uri.parse(
-        weatherUrl,
+        "https://api.openweathermap.org/data/2.5/weather?lat=$latitute&lon=$longitute&units=metric&appid=$weatherUrl",
       ),
     );
 
     if (response.statusCode == 200) {
+      // print(response.body);
       return CurrentWeather.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception("Failed to fetch current weather");
+      return throw Exception("Failed to fetch current weather");
     }
   } on Exception catch (e) {
     return throw Exception("No internet connection");
